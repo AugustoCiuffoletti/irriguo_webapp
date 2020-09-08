ddaa6417c18a21a0901a6a5a8f586c409da8575f
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-08 20:26:36 +0200

    3 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { TrentoComponent } from './trento/trento.component';
 import { PlantselectComponent } from './plantselect/plantselect.component';
 import { HistoryComponent } from './history/history.component';
 import { PlantstatComponent } from './plantselect/plantstat/plantstat.component';
@@ -11,7 +10,6 @@ import { PlantstatComponent } from './plantselect/plantstat/plantstat.component'
 @NgModule({
   declarations: [
     AppComponent,
-    TrentoComponent,
     PlantselectComponent,
     HistoryComponent,
     PlantstatComponent
