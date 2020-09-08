ab475017ba84b5c4a573d52eea507e03c24d702b
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-08 18:28:30 +0200

    3 files modified

--- src/app/plantselect/plantstat/plantstat.component.ts
+++ src/app/plantselect/plantstat/plantstat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-plantstat',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plantstat.component.css']
 })
 export class PlantstatComponent implements OnInit {
+  @Input() plant: string;
 
   constructor() { }
 
