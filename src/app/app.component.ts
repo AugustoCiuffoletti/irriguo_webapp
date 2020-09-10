b93387e4d3b6b42f191bf7c23e33b3e43dd0514e
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-10 11:26:32 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { MongodbService } from "./mongodb.service";
 
+
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
